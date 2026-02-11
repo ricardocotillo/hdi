1d1f5fc9e5efaa42da0aab685ab52c896db38cfe
Renzo Carpio Ponce <rcarpio@byteinnova.com>
2026-02-11 12:01:09 -0500

    feat: news list page

--- app/web/app/themes/blankslate-child/page.php
+++ app/web/app/themes/blankslate-child/page.php
@@ -4,8 +4,19 @@ get_header();
 if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
-		get_template_part( 'entry' );
-		comments_template();
+		
+		// Display header image
+		$header_image = carbon_get_post_meta( get_the_ID(), 'crb_page_header_image' );
+		?>
+		<?php if ( $header_image ) : ?>
+			<header class="w-full">
+				<?php echo wp_get_attachment_image( $header_image, 'full', false, array( 'class' => 'page-header-image w-full h-auto', 'alt' => get_the_title() ) ); ?>
+			</header>
+		<?php endif; ?>
+		<?php
+		
+		//get_template_part( 'entry' );
+		//comments_template();
 	endwhile;
 endif;
 
