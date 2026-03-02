db83fcd5668f0fa9dce7c74b317137f7a8dab64e
ricardocotillo <ricardo.cotillo@gmail.com>
2026-03-02 18:27:41 -0500

    feat: move "Ver más" button outside main content area in taxonomy template for improved layout structure

--- app/web/app/themes/blankslate-child/taxonomy.php
+++ app/web/app/themes/blankslate-child/taxonomy.php
@@ -90,19 +90,19 @@ get_header(); ?>
                 <?php endif; ?>
             </div>
 
-            <!-- Botón Ver Más -->
-            <?php 
-            global $wp_query;
-            if ($wp_query->found_posts > 12) :
-            ?>
-            <div class="ver-mas-container">
-                <button id="load-more-btn" class="btn-ver-mas-productos" data-paged="2" data-term-id="<?php echo $term->term_id; ?>" data-taxonomy="<?php echo $term->taxonomy; ?>">
-                    Ver más
-                </button>
-            </div>
-            <?php endif; ?>
         </main>
     </div>
+    <!-- Botón Ver Más -->
+    <?php 
+    global $wp_query;
+    if ($wp_query->found_posts > 12) :
+    ?>
+    <div class="ver-mas-container">
+        <button id="load-more-btn" class="btn-ver-mas-productos" data-paged="2" data-term-id="<?php echo $term->term_id; ?>" data-taxonomy="<?php echo $term->taxonomy; ?>">
+            Ver más
+        </button>
+    </div>
+    <?php endif; ?>
     
     <div class="w-full">
         <div class="container">
